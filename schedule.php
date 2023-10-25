3f2dc0bf28fb2a4163c095b6ae0cae958ffbbf10
NickVerwoert <506749@vistacollege.nl>
2023-10-25 11:41:23 +0200

    Main menu
    -added main menu

---
+++ schedule.php
@@ -0,0 +1,31 @@
+<?php
+    echo('
+        <html>
+            <head>
+                <link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
+                <link rel="stylesheet" type="text/css" href="css/style.css"/>
+            </head>
+            
+            <body>
+            <br/>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="scheduleMenu">
+                                <div class="headText">
+                                    <p>THE CHALLANGE ZONE</p>
+                                </div>  
+                                <div class="buttons">
+                                    <a href="#">Home</a>
+                                    <a href="#">Over ons</a>
+                                    <a href="#">Insturen</a>
+                                    <a class="login" href="#">Login <img src="./images/profile%20icon.png" alt="Profile Icon"></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </body>
+        </html>
+    ')
+?>
