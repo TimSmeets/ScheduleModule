d83d44a76117e3aeae5c35b9f68604a3313a1171
IsaiMeijer <isai.meijer@freedom.nl>
2024-01-17 12:02:31 +0100

    Prototype time table

---
+++ timetable.php
@@ -0,0 +1,54 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<style>
+    table, th, td {
+        border:1px solid black;
+    }
+</style>
+<body>
+        <table style="width:100%">
+            <tr>
+                <th style="width:20%">time</th>
+                <th style="width:80%">event</th>
+            </tr>
+            <tr>
+                <td>8:00</td>
+            </tr>
+            <tr>
+                <td>9:00</td>
+            </tr>
+            <tr>
+                <td>10:00</td>
+            </tr>
+            <tr>
+                <td>11:00</td>
+            </tr>
+            <tr>
+                <td>12:00</td>
+            </tr>
+            <tr>
+                <td>13:00</td>
+            </tr>
+            <tr>
+                <td>14:00</td>
+            </tr>
+            <tr>
+                <td>15:00</td>
+            </tr>
+            <tr>
+                <td>16:00</td>
+            </tr>
+            <tr>
+                <td>17:00</td>
+            </tr>
+            <tr>
+                <td>18:00</td>
+            </tr>
+        </table>
+</body>
+</html>
